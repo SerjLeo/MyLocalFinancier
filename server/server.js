7f43ebdda390ef985206ca582709fb14b74119bd
Sergey <sergejleontev111@gmail.com>
2019-07-12 14:58:51 +0300

    Frontend created

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
 
+// connect to DB
+connectDB(); 
+
+//Init Middleware
+app.use(express.json({
+    extended: false
+}))
+
 app.get('/', (req,res) => res.send('API running')); 
+
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/finance', require('./routes/api/finance'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
